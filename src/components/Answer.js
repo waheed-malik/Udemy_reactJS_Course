9314e135385af84c2c733689290078a1a6b6a4e3
waheed-malik <malikwaheed.1234567n@gmail.com>
2025-07-28 18:03:33 +0500

    121

---
+++ src/components/Answer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Answer() {
+  return (
+    <h2>I am Good</h2>
+  )
+}
+
+export default Answer
---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Greet() {
+  return (
+    <h1>How are you</h1>
+  )
+}
+
+export default Greet
